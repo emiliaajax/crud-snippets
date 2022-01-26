f6af66141a3bae4f2602793a9a361c56b05db9d8
Emilia Hansson <eh222yn@student.lnu.se>
2022-01-26 12:14:04 +0100

    Add mongoose configuration file according to examples from course material

---
+++ src/config/mongoose.js
@@ -0,0 +1,27 @@
+/**
+ * Mongoose configuration.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 2.0.0
+ */
+
+import mongoose from 'mongoose'
+
+/**
+ * Test.
+ *
+ * @returns {Promise} test.
+ */
+export const connectDB = async () => {
+  const { connection } = mongoose
+  connection.on('connected', () => console.log('MongoDB connection opened.'))
+  connection.on('error', err => console.error(`MongoDB connection error occurred: ${err}`))
+  connection.on('disconnected', () => console.log('MongoDB is disconnected'))
+  process.on('SIGINT', () => {
+    connection.close(() => {
+      console.log('MongoDB disconnected due to application termination')
+      process.exit(0)
+    })
+  })
+  return mongoose.connect(process.env.DB_CONNECTION_STRING)
+}
