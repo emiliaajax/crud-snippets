b024e30310cf1335e6629d0b252ca1bc05afe853
Emilia Hansson <eh222yn@student.lnu.se>
2022-01-28 16:24:51 +0100

    Start implement UserController class, add loginPost method

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,36 @@
+/**
+ * Module for the UserController.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import { User } from '../models/user.js'
+
+/**
+ *
+ */
+export class UserController {
+  /**
+   * Test.
+   *
+   * @param {*} req Test.
+   * @param {*} res Test.
+   * @param {*} next Test.
+   */
+  async loginPost (req, res, next) {
+    try {
+      const user = await User.authenticate(req.body.username, req.body.password)
+      req.session.regenerate((error) => {
+        if (error) {
+          throw new Error('Failed to regenerate session.')
+        }
+        req.session.user = user
+        res.redirect('.')
+      })
+    } catch (error) {
+      req.session.flash = { type: 'danger', text: error.message }
+      res.redirect('./login')
+    }
+  }
+}
