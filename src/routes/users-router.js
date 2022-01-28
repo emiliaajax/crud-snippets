d91798696a552bbc1a3d6a3dc8815d4403aff7b8
Emilia Hansson <eh222yn@student.lnu.se>
2022-01-28 17:13:39 +0100

    Add routes for logging in

---
+++ src/routes/users-router.js
@@ -0,0 +1,15 @@
+/**
+ * Users routes.
+ *
+ * @author Emilia Hansson <eh222yn@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import { UsersController } from '../controllers/users-controller.js'
+
+export const router = express.Router()
+const controller = new UsersController()
+
+router.get('/login', controller.login)
+router.post('/login', controller.loginPost)
